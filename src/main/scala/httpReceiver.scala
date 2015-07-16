aff3d8c57413941e749f4b06f27b0a62288a0fdd
Stefan Wachter <external.stefan.wachter@bosch-si.com>
2015-07-16 16:30:23 +0200

    set max-connections; delay response

--- src/main/scala/httpReceiver.scala
+++ src/main/scala/httpReceiver.scala
@@ -1,10 +1,15 @@
 package httpReceiver
 
+import java.util.concurrent.TimeUnit
+
 import akka.actor.{ActorRef, ActorSystem}
 import akka.event.Logging
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
+
+import scala.concurrent.duration.{Duration, FiniteDuration}
+import scala.concurrent.{Future, Promise}
 
 class HttpReceiver()(implicit val actorSystem: ActorSystem) {
 
@@ -12,10 +17,19 @@ class HttpReceiver()(implicit val actorSystem: ActorSystem) {
 
   implicit val materializer = ActorMaterializer()
 
+  val scheduler = actorSystem.scheduler
+
+  implicit val ec = actorSystem.dispatcher
+
   def doBind(interface: String, port: Int): Unit = {
 
     val route = get {
-              complete("Akka")
+              complete {
+//                "Akka"
+                val promise = Promise[String]()
+                scheduler.scheduleOnce(Duration(2000l, TimeUnit.MILLISECONDS))(promise.success("Akka"))
+                promise.future
+              }
           }
 
     Http().bindAndHandle(route, interface, port)
