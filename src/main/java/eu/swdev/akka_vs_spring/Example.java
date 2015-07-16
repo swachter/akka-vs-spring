84711b6f355b2d6007727293399995241dac6e2e
Stefan Wachter <external.stefan.wachter@bosch-si.com>
2015-07-16 18:19:03 +0200

    delay response of Spring service

--- src/main/java/eu/swdev/akka_vs_spring/Example.java
+++ src/main/java/eu/swdev/akka_vs_spring/Example.java
@@ -3,15 +3,25 @@ package eu.swdev.akka_vs_spring;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.*;
 
 @RestController
 @EnableAutoConfiguration
 public class Example {
 
+    private static final ScheduledExecutorService es = Executors.newScheduledThreadPool(10);
+
     @RequestMapping("/")
-    String home() {
-        return "Spring";
+    DeferredResult<String> home() {
+        DeferredResult<String> res = new DeferredResult<>();
+        es.schedule(() -> res.setResult("Spring"), 2000, TimeUnit.MILLISECONDS);
+        return res;
     }
 
     public static void main(String[] args) throws Exception {
